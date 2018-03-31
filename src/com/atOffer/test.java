bbb650b66d13fab36293bb6f83055ebb22ce65ee
huankai <minkai95@163.com>
2018-03-31 12:27:29 +0800

    Initial commit

---
+++ src/com/atOffer/test.java
@@ -0,0 +1,13 @@
+package com.atOffer;
+
+/**
+ * 测试类
+ * Created by huankai on 2018/3/31.
+ */
+public class test {
+
+    public static void main(String[] args) {
+        System.out.println("sad");
+    }
+
+}
